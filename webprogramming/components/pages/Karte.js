be96adde98d540a32bee6162369a27e1d5be6840
MauriceNitschke <40233071+MauriceNitschke@users.noreply.github.com>
2020-03-25 17:53:01 +0100

    Add files via upload

---
+++ webprogramming/components/pages/Karte.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Karte extends Component {
+    render() {
+        return (
+            <div>
+                <p>Karte</p>
+            </div>
+        )
+    }
+}
+
+export default Karte
